13dd7e0fd5ada7a516b64223e6c3977978a05948
Jack Davis <37071931+sexykoalajuice@users.noreply.github.com>
2022-02-08 18:48:20 +0800

    Hello Triangle is working

--- INCLUDE/Omen.h
+++ INCLUDE/Omen.h
@@ -6,5 +6,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const unsigned int SCR_WIDTH    = 800;
+const unsigned int SCR_HEIGHT   = 600;
+const char* WINDOW_TITLE = "WINDOW - TITLE";
 
 #endif//OMEN_GL
